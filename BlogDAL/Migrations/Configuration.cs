6062b2a7c5a5686abde8d1c8f7be8359438db95c
@1999Souvik <1999souvik.paul@gmail.com>
2023-12-18 21:05:49 +0530

    first commit

---
+++ BlogDAL/Migrations/Configuration.cs
@@ -0,0 +1,29 @@
+﻿namespace BlogDAL.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<BlogDAL.BlogDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "BlogDAL.BlogDbContext";
+        }
+
+        protected override void Seed(BlogDAL.BlogDbContext context)
+        {
+            var admin = new AdminInfo
+            {
+                EmailId = "1999souvik.paul@admin.com",
+                Password = "Souvik@1999"
+            };
+            context.AdminInfos.Add(admin);
+            // Add additional seeding for other tables if needed
+
+            base.Seed(context);
+        }
+    }
+}
